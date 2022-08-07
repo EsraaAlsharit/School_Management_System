eb5147da10810dae8c819a5aeaae9d6d06dd02a2
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-07 14:10:54 +0300

    student Services & Repository

--- project/src/main/java/com/Esraa/project/repositories/StudentRepository.java
+++ project/src/main/java/com/Esraa/project/repositories/StudentRepository.java
@@ -1,11 +1,14 @@
 package com.Esraa.project.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.Esraa.project.models.Student;
 
 
 
-public interface studentRepository extends CrudRepository<Student, Long>{
+public interface StudentRepository extends CrudRepository<Student, Long>{
 
+	List<Student> findAll();
 }
---
+++ project/src/main/java/com/Esraa/project/services/StudentServices.java
@@ -0,0 +1,12 @@
+package com.Esraa.project.services;
+
+import org.springframework.stereotype.Service;
+
+import com.Esraa.project.repositories.StudentRepository;
+
+@Service
+public class StudentServices {
+	
+	StudentRepository studentRepository;
+	
+}

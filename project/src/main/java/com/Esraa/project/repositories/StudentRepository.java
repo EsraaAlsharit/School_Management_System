e019f66ff9dee5d29ec189f1a4049a6ae510a0ba
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-07 14:48:39 +0300

    edit

--- project/src/main/java/com/Esraa/project/repositories/StudentRepository.java
+++ project/src/main/java/com/Esraa/project/repositories/StudentRepository.java
@@ -3,9 +3,6 @@ package com.Esraa.project.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-
-import com.Esraa.ProjectManager.models.Project;
-import com.Esraa.ProjectManager.models.User;
 import com.Esraa.project.models.Student;
 
 

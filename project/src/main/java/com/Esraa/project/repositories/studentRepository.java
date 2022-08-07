f10d8224e85045cbfd55cd291bf18ac968c33962
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-07 14:01:16 +0300

    package

---
+++ project/src/main/java/com/Esraa/project/repositories/studentRepository.java
@@ -0,0 +1,11 @@
+package com.Esraa.project.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Esraa.project.models.Student;
+
+
+
+public interface studentRepository extends CrudRepository<Student, Long>{
+
+}

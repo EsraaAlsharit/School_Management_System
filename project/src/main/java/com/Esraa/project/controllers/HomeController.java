1fbcc62addaeb12d360d499bfeb853ae200fd583
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-08 10:08:55 +0300

    edit

--- project/src/main/java/com/Esraa/project/controllers/HomeController.java
+++ project/src/main/java/com/Esraa/project/controllers/HomeController.java
@@ -1,5 +1,36 @@
 package com.Esraa.project.controllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.Esraa.project.services.GuardiansService;
+import com.Esraa.project.services.StudentServices;
+import com.Esraa.project.services.TeacherServices;
+
+@Controller
 public class HomeController {
 
+    @Autowired
+    StudentServices studentService;
+
+    @Autowired
+    TeacherServices teacherService;
+
+    @Autowired
+    GuardiansService guardiansService;
+
+    @GetMapping("/")
+    public String index(Model model, HttpSession session) {
+        if (session.getAttribute("user_id") == null) {
+//             model.addAttribute("newUser", new User());
+//             model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        } else {
+            return "redirect:/";
+        }
+    }
 }
